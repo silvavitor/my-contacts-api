80c1869e2e3608bcbb1817ea78cefb4a367199d5
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2022-02-23 07:53:10 -0300

    feat: add select one contact by id from db

--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -25,10 +25,9 @@ class ContactRepository {
     return rows;
   }
 
-  findById(id) {
-    return new Promise((resolve) => resolve(
-      contacts.find((contact) => contact.id === id),
-    ));
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id=$1', [id]);
+    return row;
   }
 
   findByEmail(email) {
