5bdf4ad0a8ccfef9827751c7751742df10f4411b
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2022-03-02 01:23:14 -0300

    feat: add error handler middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -6,5 +7,9 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3132, () => console.log('Server started at 127.0.0.1:3132'));
