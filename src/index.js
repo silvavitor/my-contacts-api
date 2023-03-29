34f8c30916c5d27bd4050b54338fa6b63c9dedb1
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2023-03-29 16:05:29 -0300

    feat: use new middlewares

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 require('express-async-errors');
 
+const cors = require('./app/middlewares/cors')
+const errorHandler = require('./app/middlewares/errorHandler')
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
-app.use((error, request, response, next) => {
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
 app.listen(3132, () => console.log('Server started at 127.0.0.1:3132'));
